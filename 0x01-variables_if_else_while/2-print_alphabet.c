66c03c7ab3372d82d3cfa6eb30073385c84a9c6e
Ashlame <mercyashlamesunday@gmail.com>
2023-10-07 17:49:38 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return 0 ()
+ *
+ */
+
+char main()
+
+{
+	char alphabet="abcdefghijklmnopqrstuvwxyz";
+
+	alphabet = 'a'
+		putchar("Lowercase alphabet\n", a);
+	while (alphabet<='z')
+	{
+		putchar("%c", alphabet);
+	}
+	Return 0()
+}
+
