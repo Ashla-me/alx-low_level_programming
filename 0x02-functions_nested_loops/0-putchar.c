b8bac5de4e432e080a72aa235fa580e8f0591223
Ashlame <mercyashlamesunday@gmail.com>
2023-10-10 14:00:51 -0700

    creating my prototype

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ *
+ */
+
+void _putchar ()
+{
+	printf(variables);
+}
+
+void print_alphabet(void)
+{
+	printf(alphabet in lowercase);
+}
+
+void print_alphabet_x10(void)
+{
+	printf(alphabet x10 in lowercase);
+}
+void _islower(void)
+{
+	printf(check);
+}
