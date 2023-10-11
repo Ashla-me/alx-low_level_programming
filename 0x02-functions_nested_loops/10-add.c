11896444d31a4b4aa8f5bbc79dfde95a5a8a1089
Ashlame <mercyashlamesunday@gmail.com>
2023-10-11 15:01:07 -0700

    prints two integer

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - it returns a sum of its parameter
- * a: int type number
- * b: int type number
+ * Description: a= int type number, b= int type number
  * Return: Always 0
  */
 int add(int a, int b)
+
+	a = int a
+	b = int b
 
 {
 	return (a + b);
