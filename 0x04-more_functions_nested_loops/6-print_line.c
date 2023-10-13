7b5eea2673d13cdc6dbc0a357175d87c13904523
Ashlame <mercyashlamesunday@gmail.com>
2023-10-13 06:21:19 -0700

    Draws a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,7 +5,9 @@
  * @n: number of times the character _ should be printed
  */
 void print_line(int n)
+
 {
+
 if (n <= 0)
 {
 _putchar('\n');
@@ -17,6 +19,8 @@ for (i = 1; i <= n; i++)
 {
 _putchar('_');
 }
+
 _putchar('\n');
 }
+
 }
