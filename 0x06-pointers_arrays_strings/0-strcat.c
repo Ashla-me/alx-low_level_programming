005bcd4d4e903b25929fd69bfc6602e60d616b9f
Ashlame <mercyashlamesunday@gmail.com>
2023-10-20 16:21:40 -0700

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcat(char *dest, char *src) - a function that concatenates two strings
+ *
+ * @src: String to be appended to dest
+ * 
+ * @dest: string to be appended upon by src
+ *
+ * @\0: adds a terminating null byte
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src);
+
+{
+	int a = 0,
+	    int b = 0,
+
+	    if a = b
+		    _putchar("\0");
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * char *_strncat(char *dest, char *src, int n) - concatenates two strings.
+ * @src: String to append to dest
+ * @dest: String to append src upon
+ * n: number of bytes from src
+ * Return:  pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int index = 0;
+int dest_len = 0;
+
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++);
+dest[dest_len++] = src[index];
+
+return (dest);
+}
