6c17fbd9e2a05e37144e5be8c9d0ce945d6fd5d5
Ashlame <mercyashlamesunday@gmail.com>
2023-10-31 07:29:04 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,14 +8,14 @@
 int count_word(char *s)
 {
 int flag, c, w;
-flag = 0;	
+flag = 0;
 w = 0;
 
 for (c = 0; s[c] != '\0'; c++)
 {
-if (s[c] == ' ')										flag = 0;									else if (flag == 0)
+if (s[c] == ' ')									flag = 0;									else if (flag == 0)
 		{
-										flag = 1;									w++;
+			flag = 1;									w++;
 		}
 }
 return (w);
@@ -27,11 +27,11 @@ return (w);
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-char **strtow(char *str)		
+char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
-		
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
@@ -40,7 +40,7 @@ char **strtow(char *str)
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)	
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
@@ -62,5 +62,5 @@ char **strtow(char *str)
 			start = i;
 	}
 	matrix[k] = NULL;
-	return (matrix);	
+	return (matrix);
 }
