4ee3cd38627caf90ea816c105b6ccbea8b1bce6c
Ashlame <mercyashlamesunday@gmail.com>
2023-10-31 07:32:06 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,7 +13,8 @@ w = 0;
 
 for (c = 0; s[c] != '\0'; c++)
 {
-if (s[c] == ' ')									flag = 0;									else if (flag == 0)
+if (s[c] == ' ')
+	flag = 0;									else if (flag == 0)
 		{
 			flag = 1;									w++;
 		}
