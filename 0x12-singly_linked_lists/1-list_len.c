ebf99ebe2bb2ee0a9feb59273a8d4de47237a00f
Ashlame <mercyashlamesunday@gmail.com>
2023-11-16 22:17:24 -0800

    corrected work

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t a = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h)
+	{
+		a++;
+		h = h->next;
+	}
+	return (a);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,16 @@
  * @head: list_t list to be freed
  */
 void free_list(list_t *head)
-{
- list_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+{
+
+list_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
 }
